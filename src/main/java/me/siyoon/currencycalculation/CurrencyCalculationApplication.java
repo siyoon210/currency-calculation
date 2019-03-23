86a0dcd77882e65d36fcf241cf6262f68685fceb
siyoon210 <siyoon210@gmail.com>
2019-03-23 11:53:02 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/me/siyoon/currencycalculation/CurrencyCalculationApplication.java
@@ -0,0 +1,13 @@
+package me.siyoon.currencycalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyCalculationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyCalculationApplication.class, args);
+    }
+
+}
---
+++ src/main/java/me/siyoon/currencycalculation/controller/MainController.java
@@ -0,0 +1,12 @@
+package me.siyoon.currencycalculation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping("/hello")
+    private String hello() {
+        return "HelloWorld~~";
+    }
+}
