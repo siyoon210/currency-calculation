4cbd348cf9905db752eb4187bcab1914bdadd458
siyoon210 <siyoon210@gmail.com>
2019-03-23 16:50:00 +0900

    RestController 환율 가져오기 구현

--- src/main/java/me/siyoon/currencycalculation/controller/CurrencyCalculationRestController.java
+++ src/main/java/me/siyoon/currencycalculation/controller/CurrencyCalculationRestController.java
@@ -1,13 +1,23 @@
 package me.siyoon.currencycalculation.controller;
 
 import lombok.RequiredArgsConstructor;
-import me.siyoon.currencycalculation.service.CurrencyInfoAPIService;
+import me.siyoon.currencycalculation.service.CurrencyCalculationService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class CurrencyCalculationRestController {
-    private final CurrencyInfoAPIService currencyInfoAPIService;
+    private final CurrencyCalculationService currencyCalculationService;
+
+    @GetMapping("/exchange-rate")
+    public ResponseEntity getExchangeRate(@RequestParam String sendingCountry, @RequestParam String receivingCountry) {
+        Double exchangeRate = currencyCalculationService.getExchangeRate(sendingCountry, receivingCountry);
+        return ResponseEntity.ok(exchangeRate);
+    }
+
 }
