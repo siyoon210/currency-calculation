1b1055edb51eb0be66fa37e6ab64e8f43e3d551a
siyoon210 <siyoon210@gmail.com>
2019-03-25 19:42:51 +0900

    fix: 숫자 포맷 0. 단위 표시하도록 수정

--- src/main/java/me/siyoon/currencycalculation/util/NumberFormatUtil.java
+++ src/main/java/me/siyoon/currencycalculation/util/NumberFormatUtil.java
@@ -3,7 +3,7 @@ package me.siyoon.currencycalculation.util;
 import java.text.DecimalFormat;
 
 public class NumberFormatUtil {
-    private static final DecimalFormat decimalFormat = new DecimalFormat("#,###.00");
+    private static final DecimalFormat decimalFormat = new DecimalFormat("#,##0.00");
 
     public static String convert(Number number) {
         return decimalFormat.format(number);
