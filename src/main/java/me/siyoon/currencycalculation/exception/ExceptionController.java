96c8c8574cf60e4ab7adef20a7c70b709a5ed36f
siyoon210 <siyoon210@gmail.com>
2019-03-26 15:20:13 +0900

    feat: Exception 컨트롤러 생성 & 인덱스의 기본 환율정보 JS에서 해결

--- src/main/java/me/siyoon/currencycalculation/exception/ExceptionController.java
+++ src/main/java/me/siyoon/currencycalculation/exception/ExceptionController.java
@@ -1,8 +1,27 @@
 package me.siyoon.currencycalculation.exception;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionHandler {
+@Slf4j
+public class ExceptionController extends ResponseEntityExceptionHandler {
 
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseEntity handleAllExceptions(Exception e) {
+        log.warn(e.getMessage());
+        return ResponseEntity.ok(e.getLocalizedMessage());
+    }
+
+    @ExceptionHandler(RestClientException.class)
+    public String handleRestClientException(RestClientException e) {
+        log.warn(e.getMessage());
+        return "index";
+    }
 }
