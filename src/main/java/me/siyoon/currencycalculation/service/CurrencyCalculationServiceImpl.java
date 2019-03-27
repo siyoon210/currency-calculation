10586971a9f3a1fa5e76add94cfd29c9fdd69c47
siyoon210 <siyoon210@gmail.com>
2019-03-27 16:59:29 +0900

    refactor: 송금국가가 source에 명시된 국가가 아닌 경우 수행한 코드에 주석 추가

--- src/main/java/me/siyoon/currencycalculation/service/CurrencyCalculationServiceImpl.java
+++ src/main/java/me/siyoon/currencycalculation/service/CurrencyCalculationServiceImpl.java
@@ -19,7 +19,7 @@ public class CurrencyCalculationServiceImpl implements CurrencyCalculationServic
 
         if (sendingCountry.equals(sourceCountry)) {
             return currencyInfo.getQuotes().get(sendingCountry + receivingCountry);
-        } else {
+        } else { //송금국가가 source 에 명시된 국가가 아닐 경우, 변환 연산하여 반환
             return currencyInfo.getQuotes().get(sourceCountry + receivingCountry)
                     / currencyInfo.getQuotes().get(sourceCountry + sendingCountry);
         }
